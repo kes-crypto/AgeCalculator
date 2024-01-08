68b56d2c2d12f4942ba2c53b2cb52d3662853c02
Dennis Kestone <denniskestone@gmail.com>
2024-01-08 06:19:05 +0300

    JS added

---
+++ script.js
@@ -0,0 +1,7 @@
+var year = document.getElementById("year");
+var month = document.getElementById("month");
+var day = document.getElementById("day");
+
+var output__year = document.getElementById("display-years");
+var output__month = document.getElementById("display-month");
+var output__day = document.getElementById("display-day");
