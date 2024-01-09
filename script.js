c852ef690792aa0485d9f8e81764f8966bd275a4
Dennis Kestone <denniskestone@gmail.com>
2024-01-09 21:34:00 +0300

    error messages

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 var year = document.getElementById("year");
 var month = document.getElementById("month");
 var day = document.getElementById("day");
+var submit = document.getElementById("image");
 
 var output__year = document.getElementById("display-years");
 var output__month = document.getElementById("display-month");
 var output__day = document.getElementById("display-day");
+
+const answers = new Date();
+
+day.addEventListener("input", () => {
+  nextFunction();
+});
+
+month.addEventListener("input", () => {
+  nextFunction();
+});
